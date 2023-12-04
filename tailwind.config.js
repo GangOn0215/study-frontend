37a976f9e29dda733ee388cc7cccf0d06e7f2884
gangon <ekdldktnekf@gmail.com>
2023-12-04 23:16:54 +0900

    [INIT] Tailwind CSS
    Not use CDN tailwind so I'm try Install use NPM is tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./Project/laravel/layout.html",
+    "./Project/laravel/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
